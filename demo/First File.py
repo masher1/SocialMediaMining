e49c9f71450c1e7956f717eaff26f93a415ad610
masher1 <masher@syr.edu>
2020-01-24 23:39:13 -0500

    Added beginner code for Number Guessing game

--- demo/First File.py
+++ demo/First File.py
@@ -1,3 +1,23 @@
-print("Hello World");
-x = int(4*4/2);
-print("The answer is: ", x);
+import random # importing "random" for random operations
+import time # importing "time" for realistic operations
+
+print("Welcome to the program!")
+time.sleep(1);
+"""This program was designed as a Guess the Number Game"""
+"""You will attempt to guess the number that the computer is thinking of between the range of 1 and 100"""
+
+for x in range(0, 10):
+    print("hmmm")
+    randomNum = random.randrange(1, 100)
+    time.sleep(.5);
+    print("I have now chosen a number!")
+    print("Let's play!")
+    usrNum = int(input("Enter the number you think it is, or press 0 to quit: "))
+    print("You chose: ", usrNum)
+    if 0 == usrNum:
+        exit()
+    usrAnswr = int(input("My number is higher than : "))
+    print("You chose: ", usrAnswr)
+    if 0 == usrNum:
+        exit()
+    #print(randomNum)
